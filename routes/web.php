6e54048648128ba6c4a54d0322d7eb584c8e8c5e
Jorge Alfaro <jorgealfaro@grupoicarus.com.mx>
2022-09-09 10:25:53 -0500

    size update img in validate field

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,20 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::get('/hello', function (){
+//   return response( 'Hello world',200)
+//       ->header('Content-Type', 'text/plain');
+    return redirect('/world');
+});
+
+Route::get('/world', function (){
+   return response( 'World Hell',200)
+       ->header('Content-Type', 'text/plain');
+});
+
+
 Route::resource('empleado',EmpleadoController::class)->middleware('auth');
-Auth::routes(['register'=>false,'reset'=>false]); // para que desaparesca registro y recuperacion del login
+Auth::routes(['register'=>true,'reset'=>false]); // para que desaparesca registro y recuperacion del login
 
 Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
 
